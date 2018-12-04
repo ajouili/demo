35f198e8de0bef566d470830f40158f33eeeac00
jouiliBigData <abdallahjouili@gmail.com>
2018-12-04 12:54:44 +0100

    Ajouter le CrossOringin

--- src/main/java/org/sid/web/ContactRestService.java
+++ src/main/java/org/sid/web/ContactRestService.java
@@ -8,28 +8,36 @@ import org.sid.entities.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+//permet d’indiquer au navigateur les domaines autorisés à recevoir une réponse du serveur.
+@CrossOrigin("*")
 public class ContactRestService {
  
 	@Autowired
 	private ContactRepository contactRepository;
 	@RequestMapping(value="/contacts",method=RequestMethod.GET)
+	@ResponseBody
 	public List<Contact> getContacts(){
 		return contactRepository.findAll();
 	}
 	@RequestMapping(value="/contacts/{id}",method=RequestMethod.GET)
+	@ResponseBody
 	public Contact getContact(@PathVariable Long id){
 		return contactRepository.findOne(id);
 	}
 	
 	@RequestMapping(value="/chercherContacts",method=RequestMethod.GET)
+	@ResponseBody
 	public Page<Contact> chercher(@RequestParam(name="mc", defaultValue="") String mc, 
 			@RequestParam(name="page", defaultValue="0")int page,
 			@RequestParam(name="size", defaultValue="5")int size){
@@ -37,17 +45,20 @@ public class ContactRestService {
 	}
 	
 	@RequestMapping(value="/contacts",method=RequestMethod.POST)
+	@ResponseBody
 	public Contact saveContact(@RequestBody Contact c){
 		return contactRepository.save(c);
 	}
 	
 	@RequestMapping(value="/contacts/{id}",method=RequestMethod.DELETE)
+	@ResponseBody
 	public boolean supprimer(@PathVariable Long id){
 		contactRepository.delete(id);
 		return true;
 	}
 	
 	@RequestMapping(value="/contacts/{id}",method=RequestMethod.PUT)
+	@ResponseBody
 	public Contact saveContact(@PathVariable Long id,@RequestBody Contact c){
 		c.setId(id);
 		return contactRepository.save(c);
